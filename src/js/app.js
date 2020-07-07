f86f7e591e7350e8292cf353f50c5c3a9dd1f64f
Charlie Rosenbury <charlie@selfinteractive.com>
2020-07-07 09:06:29 -0500

    add cobra trademark, search in strong tags

--- src/js/app.js
+++ src/js/app.js
@@ -1,87 +1,122 @@
 /*
 @preserve
-v1.0.2
+v1.0.3
 */
 (function (jQuery) {
-  var cssRules = '.gt-times {'+
+  var cssRules =
+    ".gt-times {" +
     'font-family: "times new roman";' +
-    'font-style: normal !important;' +
-    'font-weight: bold;' +
-    'text-transform: uppercase;' +
-  '}' +
-  '.gt-times .gt-times sup {' +
-    'display: none;' +
-  '}';
+    "font-style: normal !important;" +
+    "font-weight: bold;" +
+    "text-transform: uppercase;" +
+    "}" +
+    ".gt-corsiva {" +
+    'font-family: "Monotype Corsiva W01";' +
+    "font-style: normal !important;" +
+    "font-weight: bold;" +
+    "}" +
+    ".gt-times .gt-times sup {" +
+    "display: none;" +
+    "}";
 
-  var regTypes = ['GYROTONIC EXPANSION SYSTEM', 'GYROTONIC', 'GYROKINESIS', 'GYROTONER'];
-  var regContainers = ['span', 'p', 'a', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'li'];
-  var regBreakout = ['strong'];
+  var regTypes = {
+    "gt-times": [
+      "GYROTONIC EXPANSION SYSTEM",
+      "GYROTONIC",
+      "GYROKINESIS",
+      "GYROTONER",
+    ],
+    "gt-corsiva": ["Cobra"],
+  };
+  var regContainers = [
+    "strong",
+    "span",
+    "p",
+    "a",
+    "h1",
+    "h2",
+    "h3",
+    "h4",
+    "h5",
+    "h6",
+    "li",
+  ];
+  var regBreakout = ["strong"];
 
   function replaceTrademarks() {
-    jQuery(function($){
+    jQuery(function ($) {
+      $("body").append("<style>" + cssRules + "</style>");
 
-      $('body').append('<style>' + cssRules + '</style>');
+      $.each(regTypes, function (font, termList) {
+        $.each(termList, function (i, term) {
+          var containerSelectors = [];
+          var containerSelectorsNoReg = [];
 
-      $.each(regTypes, function (i, term) {
-        var containerSelectors = [];
-        var containerSelectorsNoReg = [];
+          var termRre = new RegExp("(?<!<[^>]*)" + term + "®", "g");
+          var termRe = new RegExp("(?<!<[^>]*)" + term, "g");
 
-        var termRre = new RegExp("(?<!<[^>]*)" + term + "®", "g");
-        var termRe = new RegExp("(?<!<[^>]*)" + term, "g");
+          function replaceInEl(re, contains) {
+            return function () {
+              // make sure we actually need to replace this one by seeing if it
+              // has TMs outside of its children's content
+              var $clone = $(this).clone();
+              $clone.find("*").remove();
+              if (!$clone.is(contains)) return;
 
-        function replaceInEl(re, contains) {
-          return function(){
-            // make sure we actually need to replace this one by seeing if it
-            // has TMs outside of its children's content
-            var $clone = $(this).clone();
-            $clone.find('*').remove();
-            if(!$clone.is(contains)) return;
-
-            var text = $(this).html();
-            text = text.replace(
-              re,
-              '<span class="gt-times">' + term + "<sup>®</sup></span>"
-            );
-            $(this).html(text);
+              var text = $(this).html();
+              text = text.replace(
+                re,
+                '<span class="' + font + '">' + term + "<sup>®</sup></span>"
+              );
+              $(this).html(text);
+            };
           }
-        }
 
-        $.each(regContainers, function (j, container) {
-          containerSelectors.push(container + ':contains(' + term + '®),' + container + ':contains(' + term + '&reg;)');
-          containerSelectorsNoReg.push(container + ':contains(' + term + ')');
+          $.each(regContainers, function (j, container) {
+            containerSelectors.push(
+              container +
+                ":contains(" +
+                term +
+                "®)," +
+                container +
+                ":contains(" +
+                term +
+                "&reg;)"
+            );
+            containerSelectorsNoReg.push(container + ":contains(" + term + ")");
+          });
+
+          $(containerSelectors.join(",")).each(
+            replaceInEl(
+              termRre,
+              ":contains(" + term + "®),:contains(" + term + "&reg;)"
+            )
+          );
+          $(containerSelectorsNoReg.join(","))
+            .not("." + font)
+            .each(replaceInEl(termRe, ":contains(" + term + ")"));
         });
-
-        $(containerSelectors.join(",")).each(
-          replaceInEl(
-            termRre,
-            ":contains(" + term + "®),:contains(" + term + "&reg;)"
-          )
-        );
-        $(containerSelectorsNoReg.join(","))
-          .not(".gt-times")
-          .each(replaceInEl(termRe, ':contains(' + term + ')'));
       });
     });
   }
 
   var injected = false;
-  function _injectJquery(){
-    if(injected) return;
+  function _injectJquery() {
+    if (injected) return;
     function l(u, i) {
       var d = document;
       if (!d.getElementById(i)) {
-        var s = d.createElement('script');
+        var s = d.createElement("script");
         s.src = u;
         s.id = i;
         d.body.appendChild(s);
       }
       injected = true;
     }
-    l('//code.jquery.com/jquery-3.2.1.min.js', 'jquery');
+    l("//code.jquery.com/jquery-3.2.1.min.js", "jquery");
   }
 
   function _init() {
-
     // find jquery
     if (window.$ && window.$() && window.$().jquery) {
       jQuery = window.$;
@@ -92,15 +127,14 @@ v1.0.2
     }
 
     // allow end user to disable auto-replace with window.GT_MANUAL constant
-    if(!window.GT_MANUAL) {
+    if (!window.GT_MANUAL) {
       replaceTrademarks();
     }
-
   }
 
   setTimeout(_init, 1);
 
   window.gyrotonicTrademarks = {
-    apply: replaceTrademarks
-  }
+    apply: replaceTrademarks,
+  };
 })(window.jQuery);
